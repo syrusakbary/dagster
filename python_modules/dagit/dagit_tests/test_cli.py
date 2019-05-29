275d89ac44e9c8b9342244c9f3ccc46a4263b738
Nicholas Schrock <schrockn@elementl.com>
2019-05-29 13:36:43 -0700

    Test for module name in dagit cli
    Summary:
    This confirms that https://github.com/dagster-io/dagster/issues/1359 is
    actually fixed and that we throw a ModuleNotFoundError when the module name
    is misspecified from the command line
    
    Resolves #1359
    
    Test Plan: Unit test
    
    Reviewers: max, natekupp
    
    Differential Revision: https://dagster.phacility.com/D241

--- python_modules/dagit/dagit_tests/test_cli.py
+++ python_modules/dagit/dagit_tests/test_cli.py
@@ -1,9 +1,22 @@
 from click.testing import CliRunner
 
 from dagit.cli import ui
+from dagster.utils import file_relative_path
 
 
 def test_invoke_ui():
     runner = CliRunner()
     result = runner.invoke(ui, ['--version'])
     assert 'dagit, version' in result.output
+
+
+def test_invoke_with_bad_module_name_in_repository_yaml():
+    runner = CliRunner()
+    result = runner.invoke(ui, ['-y', file_relative_path(__file__, 'repository_bad_module.yaml')])
+    assert 'ModuleNotFoundError' in str(result)
+
+
+def test_invoke_with_bad_module_name_in_cli():
+    runner = CliRunner()
+    result = runner.invoke(ui, ['-m', 'kjdfkdjfd', '-n', 'foo'])
+    assert 'ModuleNotFoundError' in str(result)
