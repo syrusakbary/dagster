50473deffa473fb5f57ab85dd105b8e90ab4a1e8
Nicholas Schrock <schrockn@elementl.com>
2019-05-28 18:07:15 -0700

    Put all dagster examples under test for presets
    Summary: This sets up the infrastructure for invoking dagster examples
    from dagster-graphql tests and then uses it to test and fix presets
    
    Test Plan: Unit tests
    
    Differential Revision: https://dagster.phacility.com/D229
    
    Reviewers: alangenfeld

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_presets.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_presets.py
@@ -1,10 +1,11 @@
-from .setup import execute_dagster_graphql, define_context
+from .setup import execute_dagster_graphql, define_context, define_examples_context
 
 
-def execute_preset_query(pipeline_name):
+def execute_preset_query(pipeline_name, context):
     pipeline_query = '''
         query PresetsQuery($name: String!) {
             pipeline(params: { name: $name }) {
+                name
                 presets {
                     __typename
                     name
@@ -16,18 +17,16 @@ def execute_preset_query(pipeline_name):
         }
     '''
 
-    return execute_dagster_graphql(
-        define_context(), pipeline_query, variables={'name': pipeline_name}
-    )
+    return execute_dagster_graphql(context, pipeline_query, variables={'name': pipeline_name})
 
 
 def test_basic_preset_query_no_presets():
-    result = execute_preset_query('csv_hello_world_two')
+    result = execute_preset_query('csv_hello_world_two', define_context())
     assert result.data == {'pipeline': {'presets': []}}
 
 
 def test_basic_preset_query_with_presets(snapshot):
-    result = execute_preset_query('csv_hello_world')
+    result = execute_preset_query('csv_hello_world', define_context())
 
     assert [preset_data['name'] for preset_data in result.data['pipeline']['presets']] == [
         'prod',
@@ -35,3 +34,9 @@ def test_basic_preset_query_with_presets(snapshot):
     ]
 
     snapshot.assert_match(result.data)
+
+
+def test_presets_on_examples(snapshot):
+    context = define_examples_context()
+    for pipeline_name in context.repository_definition.pipeline_names:
+        snapshot.assert_match(execute_preset_query(pipeline_name, context))
