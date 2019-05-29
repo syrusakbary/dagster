4dde04d5ff4a4d03a5e4e95a8b9e12eabfc849c4
Nicholas Schrock <schrockn@elementl.com>
2019-05-28 20:06:29 -0700

    (working-examples 3/N) Properly capture error in executionPlan root at top-level
    Summary:
    Without this change, a config error in the execution console totally borked
    the dagit ui. This error now appropriately captured.
    
    Test Plan: Unit tests. Verify fix in the dagit.
    
    Reviewers: natekupp
    
    Differential Revision: https://dagster.phacility.com/D231

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_presets.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_presets.py
@@ -7,6 +7,36 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
+snapshots['test_basic_preset_query_with_presets 1'] = {
+    'pipeline': {
+        'name': 'csv_hello_world',
+        'presets': [
+            {
+                '__typename': 'PipelinePreset',
+                'environmentConfigYaml': '''solids:
+  sum_solid:
+    inputs:
+      num: data/num_prod.csv
+''',
+                'mode': 'default',
+                'name': 'prod',
+                'solidSubset': None
+            },
+            {
+                '__typename': 'PipelinePreset',
+                'environmentConfigYaml': '''solids:
+  sum_solid:
+    inputs:
+      num: data/num.csv
+''',
+                'mode': 'default',
+                'name': 'test',
+                'solidSubset': None
+            }
+        ]
+    }
+}
+
 snapshots['test_presets_on_examples 1'] = {
     'pipeline': {
         'name': 'sleepy',
