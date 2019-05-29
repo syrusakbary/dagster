fc9794a27c900afe18cfa0ab29e0fdbb5fea86ef
Nate Kupp <nate@elementl.com>
2019-05-28 22:20:54 -0700

    Dask and docker-compose
    Summary:
    
    Test Plan: yolo
    
    Reviewers:
    
    Subscribers:

---
+++ python_modules/dagster-dask/dagster_dask_tests/dask-docker/build.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# For the avoidance of doubt, this script is meant to be run with the
+# test_project directory as pwd.
+# The filesystem manipulation below is to support installing local development
+# versions of dagster-graphql, dagster-aws, dagster-dask, and dagster.
+
+set -eux
+
+function cleanup {
+    rm -rf dagster
+    rm -rf dagster-graphql
+    rm -rf dagster-dask
+    rm -rf dagster-aws
+    rm -rf examples
+}
+# ensure cleanup happens on error or normal exit
+trap cleanup EXIT
+
+cp -R ../../../dagster .
+cp -R ../../../dagster-graphql .
+rsync -av --progress ../../../dagster-dask . --exclude dagster_dask_tests
+cp -R ../../../libraries/dagster-aws .
+cp -R ../../../../examples .
+
+
+rm -rf \
+  dagster/*.egg-info \
+  dagster/.tox \
+  dagster/build \
+  dagster/dist \
+  dagster-graphql/*.egg-info \
+  dagster-graphql/.tox \
+  dagster-graphql/build \
+  dagster-graphql/dist \
+  dagster-dask/*.egg-info \
+  dagster-dask/.tox \
+  dagster-dask/build \
+  dagster-dask/dist \
+  dagster-aws/*.egg-info \
+  dagster-aws/.tox \
+  dagster-aws/build \
+  dagster-aws/dist || true
+
+docker build . --build-arg PYTHON_VERSION=$1 -t dagster-dask-test:py$1
---
+++ python_modules/dagster-dask/dagster_dask_tests/test_cluster.py
@@ -0,0 +1,127 @@
+import random
+import time
+
+from dagster import (
+    DependencyDefinition,
+    Dict,
+    ExecutionTargetHandle,
+    Field,
+    InputDefinition,
+    Int,
+    ModeDefinition,
+    OutputDefinition,
+    PipelineDefinition,
+    Result,
+    SolidInstance,
+    RunConfig,
+    lambda_solid,
+    solid,
+    RunStorageMode,
+)
+from dagster_dask import execute_on_dask, DaskConfig
+
+
+@solid(
+    inputs=[InputDefinition('chase_duration', Int)],
+    outputs=[OutputDefinition(Int, 'total')],
+    config_field=Field(
+        Dict(
+            fields={
+                'chase_size': Field(
+                    Int,
+                    default_value=100000,
+                    is_optional=True,
+                    description='How big should the pointer chase array be?',
+                )
+            }
+        )
+    ),
+)
+def hammer(context, chase_duration):
+    '''what better way to do a lot of gnarly work than to pointer chase?'''
+    ptr_length = context.solid_config['chase_size']
+
+    data = list(range(0, ptr_length))
+    random.shuffle(data)
+
+    curr = random.randint(0, ptr_length)
+    # and away we go
+    start_time = time.time()
+    while (time.time() - start_time) < chase_duration:
+        curr = data[curr]
+
+    context.log.info('Hammered - start %d end %d' % (start_time, time.time()))
+    return chase_duration
+
+
+@solid(
+    config_field=Field(Int, is_optional=True, default_value=1),
+    outputs=[
+        OutputDefinition(Int, 'out_1'),
+        OutputDefinition(Int, 'out_2'),
+        OutputDefinition(Int, 'out_3'),
+        OutputDefinition(Int, 'out_4'),
+    ],
+)
+def giver(context):
+    chase_duration = context.solid_config
+
+    yield Result(chase_duration, 'out_1')
+    yield Result(chase_duration, 'out_2')
+    yield Result(chase_duration, 'out_3')
+    yield Result(chase_duration, 'out_4')
+
+
+@lambda_solid(
+    inputs=[
+        InputDefinition('in_1', Int),
+        InputDefinition('in_2', Int),
+        InputDefinition('in_3', Int),
+        InputDefinition('in_4', Int),
+    ],
+    output=OutputDefinition(Int),
+)
+def total(in_1, in_2, in_3, in_4):
+    return in_1 + in_2 + in_3 + in_4
+
+
+def define_hammer_pipeline():
+    return PipelineDefinition(
+        name="thors_hammer",
+        solids=[giver, hammer, total],
+        dependencies={
+            SolidInstance('giver'): {},
+            SolidInstance('hammer', alias='hammer_1'): {
+                'chase_duration': DependencyDefinition('giver', 'out_1')
+            },
+            SolidInstance('hammer', alias='hammer_2'): {
+                'chase_duration': DependencyDefinition('giver', 'out_2')
+            },
+            SolidInstance('hammer', alias='hammer_3'): {
+                'chase_duration': DependencyDefinition('giver', 'out_3')
+            },
+            SolidInstance('hammer', alias='hammer_4'): {
+                'chase_duration': DependencyDefinition('giver', 'out_4')
+            },
+            SolidInstance('total'): {
+                'in_1': DependencyDefinition('hammer_1', 'total'),
+                'in_2': DependencyDefinition('hammer_2', 'total'),
+                'in_3': DependencyDefinition('hammer_3', 'total'),
+                'in_4': DependencyDefinition('hammer_4', 'total'),
+            },
+        },
+        mode_definitions=[ModeDefinition()],
+    )
+
+
+def test_dask_cluster():
+    result = execute_on_dask(
+        ExecutionTargetHandle.for_pipeline_module(
+            'dagster_examples.toys.hammer', 'define_hammer_pipeline'
+        ),
+        env_config={'storage': {'s3': {'s3_bucket': 'dagster-airflow-scratch'}}},
+        run_config=RunConfig(storage_mode=RunStorageMode.S3),
+        dask_config=DaskConfig(address='localhost:8786'),
+    )
+    assert result.success
+    assert result.result_for_solid('total').transformed_value() == 4
