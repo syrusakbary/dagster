312f97b616cf725aca08058f385e4ba4d1e2d059
Nicholas Schrock <schrockn@elementl.com>
2019-05-28 18:07:15 -0700

    (working-examples 2/N) Put all dagster examples under test for presets
    Summary: This sets up the infrastructure for invoking dagster examples
    from dagster-graphql tests and then uses it to test and fix presets
    
    Depends on D230
    
    Test Plan: Unit tests
    
    Differential Revision: https://dagster.phacility.com/D229
    
    Reviewers: alangenfeld, natekupp

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_presets.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_presets.py
@@ -2,13 +2,14 @@
 # snapshottest: v1 - https://goo.gl/zC4yUc
 from __future__ import unicode_literals
 
-from snapshottest import Snapshot
+from snapshottest import Snapshot, GenericRepr
 
 
 snapshots = Snapshot()
 
 snapshots['test_basic_preset_query_with_presets 1'] = {
     'pipeline': {
+        'name': 'csv_hello_world',
         'presets': [
             {
                 '__typename': 'PipelinePreset',
@@ -35,3 +36,25 @@ snapshots['test_basic_preset_query_with_presets 1'] = {
         ]
     }
 }
+
+snapshots['test_presets_on_examples 1'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 2'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 3'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 4'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 5'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 6'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 7'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 8'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 9'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 10'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
+
+snapshots['test_presets_on_examples 11'] = GenericRepr("<graphql.execution.base.ExecutionResult object at 0x100000000>")
